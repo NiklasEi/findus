19fc20881a1676e25c9284c0cde13fa62be424c3
Niklas Eicker <git@nikl.me>
2019-08-19 11:01:16 +0200

    rename process envs to start with GATSBY_

--- frontend/src/utils/auth/userPool.js
+++ frontend/src/utils/auth/userPool.js
@@ -1,8 +1,8 @@
 const AmazonCognitoIdentity = require("amazon-cognito-identity-js");
 
 const data = {
-  UserPoolId: process.env.AWS_COGNITO_USER_POOL_ID,
-  ClientId: process.env.AWS_COGNITO_CLIENT_ID
+  UserPoolId: process.env.GATSBY_AWS_COGNITO_USER_POOL_ID,
+  ClientId: process.env.GATSBY_AWS_COGNITO_CLIENT_ID
 };
 
 const userPool = new AmazonCognitoIdentity.CognitoUserPool(data);

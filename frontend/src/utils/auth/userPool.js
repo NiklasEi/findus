ac41134863a1d2936aa39a3aecc83c85c1e9cfa1
Niklas Eicker <git@nikl.me>
2019-08-19 10:55:51 +0200

    move public IDs to toml file

--- frontend/src/utils/auth/userPool.js
+++ frontend/src/utils/auth/userPool.js
@@ -2,7 +2,7 @@ const AmazonCognitoIdentity = require("amazon-cognito-identity-js");
 
 const data = {
   UserPoolId: process.env.AWS_COGNITO_USER_POOL_ID,
-  ClientId: process.env.AWS_COGNITO_CLIENT_ID,
+  ClientId: process.env.AWS_COGNITO_CLIENT_ID
 };
 
 const userPool = new AmazonCognitoIdentity.CognitoUserPool(data);
